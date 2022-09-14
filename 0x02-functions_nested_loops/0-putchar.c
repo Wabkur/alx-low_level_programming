e2bd972876432c04190bad7d93ddb54a2930929e
wabkur <wabilitukur@gmail.com>
2022-09-14 16:07:32 -0700

    task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,16 @@
-#include"main.h"
-
+#include "main.h"
 /**
  * main - Entry point
  *
- * Description: prints _putchar using putchar prototype
- *
- * Return: Always 0 (Success)
+ * Return: always 0(Success)
  */
-
 int main(void)
 {
-        char str[] = "_putchar";
-        int ch;
+	char str[] = "_putchar";
+	int ch;
 
-        for (ch = 0; ch < 8; ++ch)
-                _putchar(str[ch]);
-        _putchar('\n');
-
-        return (0);
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
+	_putchar('\n');
+	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include<stdio.h>
+/**
+ * File: main.h
+ * Desc: header file containing prototypes for all functions
+ *
+ */
 
 int _putchar(char c);
+void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
@@ -16,4 +21,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif /* MAIN_H */
+#endif
