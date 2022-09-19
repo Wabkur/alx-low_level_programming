f256d6c50d041cadc3c426f88659feb990d7ad97
wabkur <wabilitukur@gmail.com>
2022-09-19 15:30:40 +0100

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -5,13 +5,13 @@
  * @a: first int to swap
  * @b: second int to swap
  *
- * Return: void 
+ * Return: void
  */
-void swap_int(int *a, int *b) 
+void swap_int(int *a, int *b)
 {
 	int c;
 
 	c = *a;
 	*a = *b;
 	*b = c;
-}	
+}
