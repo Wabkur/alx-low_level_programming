39e1cd37859919ef201f40e4ec0719f7b7442586
wabkur <wabilitukur@gmail.com>
2022-09-23 10:51:01 +0100

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,20 @@
-#include <stdio.h>
-void display(int age1, int age2) {
-  printf("%d\n", age1);
-  printf("%d\n", age2);
+#include "main.h"
+
+/**
+ * *_strcat - to concatenate two strings
+ * @src: the source string to append to @dest
+ * @dest: the destiation string to be concatenated upon
+ * Return: pointer to the resulting string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int index = 0;
+	int dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
 }
-
-int main() {
-  int ageArray[] = {2, 8, 4, 12};
-
-  // pass second and third elements to display()
-  display(ageArray[1], ageArray[2]); 
-  return 0;
-}
-
