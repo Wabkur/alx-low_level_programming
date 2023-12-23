5208e92b8a478af90140661f530ff5c385fa27d5
wabkur <wabilitukur@gmail.com>
2023-12-23 12:29:19 -0800

    clear bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set the value of a bit to 0 at a given index
+ * @index: ..
+ *
+ * Return: 1 or -1
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 64)
+		return (-1);
+
+	*n &= ~(1 << index);
+	return (1);
+}
