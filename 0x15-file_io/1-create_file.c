b7e23a069e7f13d2031d507f27292fa8a8f08c5e
wabkur <wabilitukur@gmail.com>
2024-02-18 11:34:53 -0800

    file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * create_file - Create a file and fills it with text.
+ * @filename: The name of the file to create.
+ * @text_content: A text to write the file.
+ *
+ * Return: If the func. fail - -1.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+        int w, r;
+        int besh = 0;
+        if (!filename)
+                return (-1);
+        if (text_content != NULL)
+        {
+                for (besh = 0; text_content[len];)
+                        len++;
+        }
+
+        w = open(filename, 0_CREAT | 0_RDWR | 0_TRUNC, 0600);
+        r = write(w, text_content, besh);
+
+        if (w < 0 || r < 0)
+                return (-1);
+
+        close(0);
+
+        return (1);
+}
