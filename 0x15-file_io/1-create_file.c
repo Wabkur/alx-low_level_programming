d05ccdca583f1f7280686796f105331de1087839
wabkur <wabilitukur@gmail.com>
2024-03-04 12:05:08 -0800

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
         if (text_content != NULL)
         {
                 for (besh = 0; text_content[besh];)
-                        len++;
+                        besh++;
         }
 
         w = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
