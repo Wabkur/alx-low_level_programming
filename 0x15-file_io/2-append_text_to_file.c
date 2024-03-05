136391829235f1957bc6f4c0e6fb12231956517c
wabkur <wabilitukur@gmail.com>
2024-03-05 01:43:36 -0800

    append

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * append_text_to_file - Append text at the end of a file
+ * @filename: File to append the text
+ * @text_content: Content to add into the file
+ *
+ * Return: 1 if success and -1 if failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int m, t, wab = 0;
+
+	if (!filename)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (wab = 0; text_content[wab];)
+			wab++;
+	}
+
+	m = open(filename, O_WRONLY | O_APPEND);
+	t = write(m, text_content, wab);
+
+	if (m == -1 || t == -1)
+		return (-1);
+
+	close(m);
+
+	return (-1);
+}
