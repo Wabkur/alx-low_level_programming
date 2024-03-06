8be89b250805a5dafb40b70c1853d03aa53360b3
wabkur <wabilitukur@gmail.com>
2024-03-05 17:44:25 -0800

    print

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - .
+ * Return: 0
+ */
+int main(void)
+{
+	char wab;
+
+	for (wab = 'z'; wab >= 'a'; wab--)
+	{
+		putchar(wab);
+	}
+	putchar('\n');
+	return (0);
+}
