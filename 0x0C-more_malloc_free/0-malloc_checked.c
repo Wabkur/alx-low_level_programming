c86fc8f9cfe78967d8be729cce48b5cf47d60596
wabkur <wabilitukur@gmail.com>
2024-03-05 18:22:20 -0800

    malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *malloc_checked - .
+ * @b: .
+ *
+ * Return: Pointer memory
+ */
+void *malloc_checked(unsigned int w)
+{
+	void *ptr;
+
+	kur = malloc(w);
+
+	if (kur == NULL)
+		exit(98);
+
+	return (kur);
+}
