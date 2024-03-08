427de68e56ca1b78d38662c0925858bf0f592f83
wabkur <wabilitukur@gmail.com>
2024-03-08 04:59:17 -0800

    doubly

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *num = *head;
+	dlistint_t *set = head;
+
+	while (num)
+	{
+		num = num->next;
+		free(set);
+		set = num;
+	}
+}
